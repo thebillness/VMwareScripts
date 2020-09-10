9d3e7e11b3484b75815e494366d4ea7b100c3605
Bill Wall <wall.bill@gmail.com>
2020-09-10 08:56:32 -0500

    Created Fix-OrphanedVMs to automatically drop and re-add orphaned machines to the inventory.

---
+++ Fix-OrphanedVMs.ps1
@@ -0,0 +1,27 @@
+Param(
+    ## Get confirmation?
+    [bool]$Confirm = $true,
+    ## Boot the VM when we're done?
+    [Bool]$BootVM = $false   
+)
+
+# Get a View of all the VMs with an orphaned connection state
+$myView = Get-View -ViewType VirtualMachine | Where-Object {$_.Runtime.ConnectionState -eq "orphaned"}
+# Assemble the needed data from each VM in the view
+$myVMs = $myView | % {Get-VM -Name $_.Name |Select-Object Name,VMHost,FolderID,@{E={$_.ExtensionData.Config.Files.VmPathName};L="VMXPath"}}
+# Tell the user what we're about to do
+Write-Output "Preparing to act on these VMs:"
+$myVMs | Select-Object Name,VMHost,VMXPath
+# Allow the user the chance to cancel
+If ($confirm) {$null = Read-Host "Press Enter to continue"}
+# Remove the orphaned VMs from inventory
+Write-Output "Removing orphaned VMs from inventory..."
+$myVMs | ForEach-Object {Remove-VM -Confirm:$false -VM (Get-VM -Name $_.Name)}
+# Add the VMs back to inventory
+Write-Output "Adding VMs back to inventory..."
+$resultVMs = $myVMs | %{New-VM -VMFilePath $_.VMXPath -VMHost $_.VMHost -Location (Get-Folder -Id $_.FolderID)}
+# If requested, power on VMs
+If ($BootVM) {
+	Write-Output "Booting VMs..."
+	$resultVMs | Start-VM -Confirm:$false
+}
